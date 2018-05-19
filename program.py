2e3273b3e2474c2d837e7e762f19e65669b21587
iandelaney <delaney.ian@gmail.com>
2018-05-19 15:29:28 +0100

    newest changes2

--- program.py
+++ program.py
@@ -13,14 +13,15 @@ def print_header():
 def run_event_loop():
     print("What do you want to do with your journal?")
     cmd = None
+    journal_data = []
 
     while cmd != "x":
         cmd = input("[L]ist entries, [A]dd an entry, or e[x]it: ")
         cmd = cmd.lower().strip()
         if cmd == "l":
-            list_entries()
+            list_entries(journal_data)
         elif cmd == "a":
-            add_entry()
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry, we don't understand '{}'".format(cmd))
 
@@ -28,11 +29,12 @@ def run_event_loop():
     print("Done. Goodbye.")
 
 
-def list_entries():
-    print("Listing...")
+def list_entries(data):
+    print(data)
 
 
-def add_entry():
-    print("Adding...")
+def add_entry(data):
+    text = input("Type your entry. <enter> to exit: ")
+    data.append(text)
 
 main()
